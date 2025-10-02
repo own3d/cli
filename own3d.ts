8abe43ee18244345e6b1e07343b243c0d10e4fd3
René Preuß <rene@preuss.io>
2025-10-02 12:26:44 +0200

    style: Update code formatting and string quotations

--- own3d.ts
+++ own3d.ts
@@ -2,21 +2,35 @@
 // deno-lint-ignore-file
 // noinspection JSIgnoredPromiseFromCall
 
-import { Args, parseArgs } from 'https://deno.land/std@0.207.0/cli/parse_args.ts'
-import { fnCreate } from './src/commands/fnCreate.ts'
-import { fnDeploy } from './src/commands/fnDeploy.ts'
-import { login } from './src/commands/login.ts'
-import { selfUpdate } from './src/commands/selfUpdate.ts'
-import { extDeploy } from './src/commands/extDeploy.ts'
+import {
+  Args,
+  parseArgs,
+} from "https://deno.land/std@0.207.0/cli/parse_args.ts";
+import { fnCreate } from "./src/commands/fnCreate.ts";
+import { fnDeploy } from "./src/commands/fnDeploy.ts";
+import { login } from "./src/commands/login.ts";
+import { selfUpdate } from "./src/commands/selfUpdate.ts";
+import { extDeploy } from "./src/commands/extDeploy.ts";
 import { extInit } from "./src/commands/extInit.ts";
-import { ExtDeployHelp } from './src/utils.ts'
-import axios from 'npm:axios'
-import { csCreate, csTree, csLs, csRead, csWrite, csRm, csReset, csSync, csUse, csInfo } from './src/commands/codespace.ts'
+import { ExtDeployHelp } from "./src/utils.ts";
+import axios from "npm:axios";
+import {
+  csCreate,
+  csInfo,
+  csLs,
+  csRead,
+  csReset,
+  csRm,
+  csSync,
+  csTree,
+  csUse,
+  csWrite,
+} from "./src/commands/codespace.ts";
 
-const version: '0.1.0-rc.8';
+const version: string = "0.1.0-rc.8";
 
 // set user agent for axios
-axios.defaults.headers['User-Agent'] = `own3d-cli/${version}`
+axios.defaults.headers["User-Agent"] = `own3d-cli/${version}`;
 
 const help: string = `own3d ${version}
 Command line tool for OWN3D Apps.
@@ -42,69 +56,69 @@ SUBCOMMANDS:
 Global codespace resolution order for cs:* (except cs:use): --id > ENV OWN3D_CODESPACE_ID > stored default (set via cs:use or cs:create) 
 
 For more information, read the documentation at https://dev.own3d.tv/docs/cli/
-`
+`;
 
-const args: Args = parseArgs(Deno.args)
-const subcommand = args._.shift()
+const args: Args = parseArgs(Deno.args);
+const subcommand = args._.shift();
 
-if (args['help'] || args['h']) {
-    switch (subcommand) {
-        case 'ext:deploy':
-            console.log(ExtDeployHelp)
-            break
-        default:
-            console.log(help)
-            break
-    }
-    Deno.exit(0)
+if (args["help"] || args["h"]) {
+  switch (subcommand) {
+    case "ext:deploy":
+      console.log(ExtDeployHelp);
+      break;
+    default:
+      console.log(help);
+      break;
+  }
+  Deno.exit(0);
 }
 
 // noinspection FallThroughInSwitchStatementJS
 switch (subcommand) {
-    case 'ext:deploy':
-        Deno.exit(await extDeploy(args))
-    case 'ext:init':
-        Deno.exit(await extInit(args))
-    case 'fn:create':
-        Deno.exit(await fnCreate(args))
-    case 'fn:deploy':
-        Deno.exit(await fnDeploy(args))
-    case 'fn:run':
-        console.log('Running...')
-        break
-    case 'self-update':
-        Deno.exit(await selfUpdate(args))
-    case 'login':
-        Deno.exit(await login(args))
-    case 'cs:create':
-        Deno.exit(await csCreate(args))
-    case 'cs:tree':
-        Deno.exit(await csTree(args))
-    case 'cs:ls':
-        Deno.exit(await csLs(args))
-    case 'cs:read':
-        Deno.exit(await csRead(args))
-    case 'cs:write':
-        Deno.exit(await csWrite(args))
-    case 'cs:rm':
-        Deno.exit(await csRm(args))
-    case 'cs:reset':
-        Deno.exit(await csReset(args))
-    case 'cs:sync':
-        Deno.exit(await csSync(args))
-    case 'cs:use':
-        Deno.exit(await csUse(args))
-    case 'cs:info':
-        Deno.exit(await csInfo(args))
-    default:
-        if (args.version) {
-            console.log(version)
-            Deno.exit(0)
-        }
-        if (args.help) {
-            console.log(help)
-            Deno.exit(0)
-        }
-        console.error(help)
-        Deno.exit(1)
+  case "ext:deploy":
+    Deno.exit(await extDeploy(args));
+  case "ext:init":
+    Deno.exit(await extInit(args));
+  case "fn:create":
+    Deno.exit(await fnCreate(args));
+  case "fn:deploy":
+    Deno.exit(await fnDeploy(args));
+  case "fn:run":
+    console.log("Running...");
+    break;
+  case "self-update":
+    Deno.exit(await selfUpdate(args));
+  case "login":
+    Deno.exit(await login(args));
+  case "cs:create":
+    Deno.exit(await csCreate(args));
+  case "cs:tree":
+    Deno.exit(await csTree(args));
+  case "cs:ls":
+    Deno.exit(await csLs(args));
+  case "cs:read":
+    Deno.exit(await csRead(args));
+  case "cs:write":
+    Deno.exit(await csWrite(args));
+  case "cs:rm":
+    Deno.exit(await csRm(args));
+  case "cs:reset":
+    Deno.exit(await csReset(args));
+  case "cs:sync":
+    Deno.exit(await csSync(args));
+  case "cs:use":
+    Deno.exit(await csUse(args));
+  case "cs:info":
+    Deno.exit(await csInfo(args));
+  default:
+    if (args.version) {
+      console.log(version);
+      Deno.exit(0);
+    }
+    if (args.help) {
+      console.log(help);
+      Deno.exit(0);
+    }
+    console.error(help);
+    Deno.exit(1);
 }
